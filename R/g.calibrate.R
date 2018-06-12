ab03f482f50fe934a4f9008a00535fb681021493
PeteJWatson <33326094+PeteJWatson@users.noreply.github.com>
2018-06-12 09:27:04 +0000

    Update g.calibrate.R

--- R/g.calibrate.R
+++ R/g.calibrate.R
@@ -77,17 +77,21 @@ g.calibrate = function(datafile,use.temp=TRUE,spherecrit=0.3,minloadcrit=72,prin
     #process data as read from binary file
     if (length(P) > 0) { #would have been set to zero if file was corrupt or empty
       if (mon == 1) {
-        data = P$rawxyz / 1000 #convert g output to mg for genea
+        #data = P$rawxyz / 1000 #convert g output to mg for genea
+        dataj = P$rawxyz
+        data = dataj[seq(1,nrow(dataj),2),]
       } else if (mon == 4 & dformat == 3) {
         data = P$rawxyz #change scalling for Axivity?
       } else if (mon == 2  & dformat == 1) {
         # GENE monitor & Binary format (bin file)
         #data = P$data.out
-        #datai = P$data.out
+        datai = P$data.out
         data = datai[seq(1,nrow(datai),2),]
         print(data[1:10,])
       } else if (dformat == 2) {
-        data = as.matrix(P)
+        #data = as.matrix(P)
+        dataj = as.matrix(P)
+        data = dataj[seq(1,nrow(dataj),2),]
       } else if (dformat == 4) {
         data = P$data
       }
