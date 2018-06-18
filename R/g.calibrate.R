b3d1d0ae5f2a8162c0e058aa776bd1316322d475
PeteJWatson <33326094+PeteJWatson@users.noreply.github.com>
2018-06-18 10:08:23 +0000

    Update g.calibrate.R

--- R/g.calibrate.R
+++ R/g.calibrate.R
@@ -87,7 +87,7 @@ g.calibrate = function(datafile,use.temp=TRUE,spherecrit=0.3,minloadcrit=72,prin
         # GENE monitor & Binary format (bin file)
         #data = P$data.out
         datai = P$data.out
-        data = datai[seq(1,nrow(datai),4),]
+        data = datai[seq(1,nrow(datai),2),]
         #print(data[1:21,])
       } else if (dformat == 2) {
         data = as.matrix(P)
