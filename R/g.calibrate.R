fa9990240012ce688f7274d7cff4eac5066a5517
petewatson <33326094+PeteJWatson@users.noreply.github.com>
2021-11-22 21:18:02 +0000

    Update g.calibrate.R

--- R/g.calibrate.R
+++ R/g.calibrate.R
@@ -86,6 +86,7 @@ g.calibrate = function(datafile,use.temp=TRUE,spherecrit=0.3,minloadcrit=72,prin
       } else if (mon == 2  & dformat == 1) {
         # GENE monitor & Binary format (bin file)
         data = P$data.out
+        write.csv(data,file="bin.csv")
         #datai = P$data.out
         #25hz
         #data = datai[seq(1,nrow(datai),4),]
